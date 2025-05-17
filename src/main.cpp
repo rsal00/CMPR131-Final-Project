f06931b8ebd5697b180382ee44d82be3bf873762
ruben <136784798+rsal00@users.noreply.github.com>
2025-05-17 10:19:39 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <set>
 #include "ShoppingCart.h"
 using namespace std;
 
 int main()
 {
     ShoppingCart cart;
-    cart.addItem("Milk");
-    cart.addItem("Chips");
+    cart.addItem();
+    cart.addItem();
 
-    cart.listItems();
+    cout << cart.sumPrices() << endl;
 
     return 0;
 }
